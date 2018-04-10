0cab42954731aee150079ca7cd6d63554a4cce3d
Greg Biles <greg.biles@mattersight.com>
2018-04-10 09:46:55 -0500

    Now works in a running container under K8S, which doesn't allow for Console.RedKey by default.

--- src/mock-ae/Program.cs
+++ src/mock-ae/Program.cs
@@ -7,10 +7,23 @@ namespace Mattersight.mock.ba.ae
         static void Main(string[] args)
         {
             var ae = new AnalyticEngine();
-            ae.Start();
+            var workerTask = ae.Start();
 
-            Console.ReadKey(true);
-            ae.Stop();
+            TimeSpan timeout;
+
+            try
+            {
+                Console.ReadKey(true);
+                ae.Stop();
+                timeout = TimeSpan.FromSeconds(30);
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine($"{DateTime.Now} - No console detected.  I will run for 1 day.");
+                timeout = TimeSpan.FromDays(1);
+            }
+
+            workerTask.Wait(timeout);
 
         }
     }
