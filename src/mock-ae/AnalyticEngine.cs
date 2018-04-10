676f373f8b2cc8132951845e405f66b3b27c3f10
Greg Biles <greg.biles@mattersight.com>
2018-04-10 17:15:10 -0500

    Charles demo

--- src/mock-ae/AnalyticEngine.cs
+++ src/mock-ae/AnalyticEngine.cs
@@ -22,6 +22,7 @@ namespace Mattersight.mock.ba.ae
                 {
                     var sleepPeriod = TimeSpan.FromSeconds(10);
                     Console.WriteLine();
+                    Console.WriteLine($"{DateTime.Now} - Charles likes the number 27.");
                     Console.WriteLine($"{DateTime.Now} - Press any key to terminate.  I'm going to spit out messages every {sleepPeriod.TotalSeconds} seconds.");
                     do
                     {
