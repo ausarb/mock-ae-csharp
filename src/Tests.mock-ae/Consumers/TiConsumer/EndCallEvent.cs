080fdc4b0e5710a99f626c69c32c0844c97d5926
Greg Biles <greg.biles@mattersight.com>
2018-04-27 13:33:42 -0500

    Switched some tests over to xBehave.  Still not sure if I prefer this to MSpec or straight up xUnit.

---
+++ src/Tests.mock-ae/Consumers/TiConsumer/EndCallEvent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Mattersight.mock.ba.ae.Domain.Ti;
+using Mattersight.mock.ba.ae.Domain.Transcription;
+using Mattersight.mock.ba.ae.ProcessingStreams;
+using Moq;
+using Shouldly;
+using Xbehave;
+using It = Moq.It;
+
+namespace Mattersight.mock.ba.ae.Tests.Consumers.TiConsumer
+{
+    public class EndCallEvent
+    {
+        [Scenario]
+        public void When_processing_an_end_call_event(ae.Consumers.TiConsumer sut, Mock<IConsumingStream<CallEvent>> incomingStream, Mock<IProducingStream<CallTranscript>> outgoingStream, Action<CallEvent> onEventReceived)
+        {
+            "Given a TiConsumer consuming call events".x(() =>
+            {
+                incomingStream = new Mock<IConsumingStream<CallEvent>>();
+                incomingStream
+                    .Setup(x => x.Subscribe(It.IsAny<Action<CallEvent>>()))
+                    .Callback((Action<CallEvent> action) => onEventReceived = action);
+
+                outgoingStream = new Mock<IProducingStream<CallTranscript>>();
+
+                sut = new ae.Consumers.TiConsumer(incomingStream.Object, outgoingStream.Object);
+
+                // sanity check
+                onEventReceived.ShouldNotBeNull($"{sut.GetType()}.Start() should have registered something with the Subscribe method.");
+            });
+
+            "that receives an 'end call' event".x(() =>
+            {
+                onEventReceived(new CallEvent {AcdEvent = new AcdEvent {EventType = "end call"}});
+            });
+
+            "should create a transcription".x(() =>
+            {
+                outgoingStream.Verify(x => x.OnNext(It.IsAny<CallTranscript>()), Times.Once);
+            });
+        }
+    }
+}
---
+++ src/Tests.mock-ae/ProcessingStreams/RabbitMQ/ConsumingStreamSpecs/SuccessfullyProcessingSubscriber.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Mattersight.mock.ba.ae.ProcessingStreams.RabbitMQ;
+using Mattersight.mock.ba.ae.Serialization;
+using Moq;
+using RabbitMQ.Client;
+using Xbehave;
+
+// ReSharper disable ImplicitlyCapturedClosure
+
+namespace Mattersight.mock.ba.ae.Tests.ProcessingStreams.RabbitMQ.ConsumingStreamSpecs
+{
+    public class SuccessfullyProcessingSubscriber
+    {
+        [Scenario]
+        public void When_subscribers_event_proccesing_completes(ConsumingStream<object> sut, IBasicConsumer consumer, Mock<IModel> channel)
+        {
+            var ctx = new CancellationTokenSource();
+            try
+            {
+                var deliveryTag = (ulong)1234;
+
+                "Given a consuming stream".x(() =>
+                {
+                    channel = new Mock<IModel>();
+
+                    // When channel.BasicConsume is called, the consumer is passed to it, so grab it and we can simulate receiving a message
+                    channel
+                        .Setup(x => x.BasicConsume(It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>(), It.IsAny<IDictionary<string, object>>(), It.IsAny<IBasicConsumer>()))
+                        .Callback<string, bool, string, bool, bool, IDictionary<string, object>, IBasicConsumer>((foo1, foo2, foo3, foo4, foo5, foo6, theConsumer) => consumer = theConsumer)
+                        .Returns("foo");
+
+                    var connection = Mock.Of<IConnection>(x => x.CreateModel() == channel.Object);
+                    var connectionFactory = Mock.Of<IConnectionFactory>(x => x.CreateConnection() == connection);
+
+                    var deserializer = Mock.Of<IDeserializer<byte[], object>>(x => x.Deserialize(It.IsAny<byte[]>()) == new object());
+                    sut = new ConsumingStream<object>(new QueueConfiguration(), connectionFactory, deserializer);
+                    sut.Start(CancellationToken.None);
+                });
+
+                "with a successfully processing subscriber"
+                    .x(() => sut.Subscribe(x => Console.WriteLine("noop")));
+
+                "When a message is delivered"
+                    .x(() => consumer.HandleBasicDeliver("foo", deliveryTag, false, "exchange", "routingKey", Mock.Of<IBasicProperties>(), new byte[100]));
+
+                "It should ACK the message"
+                    .x(() => channel.Verify(x => x.BasicAck(deliveryTag, It.IsAny<bool>()), Times.Once));
+
+                "It should not NACK the message"
+                    .x(() => channel.Verify(x => x.BasicNack(It.IsAny<ulong>(), It.IsAny<bool>(), It.IsAny<bool>()), Times.Never()));
+            }
+            finally
+            {
+                ctx.Cancel();
+            }
+        }
+    }
+}
