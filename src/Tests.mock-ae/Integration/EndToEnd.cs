68921288707eb20e1513a914b7e2262e89a4c90d
Greg Biles <greg.biles@mattersight.com>
2018-05-01 10:50:34 -0500

    Added some console writes to prove we're starting the silo as the normal logging stuff isn't showing up in AppVeyor during the test runs.

--- src/Tests.mock-ae/Integration/EndToEnd.cs
+++ src/Tests.mock-ae/Integration/EndToEnd.cs
@@ -85,7 +85,9 @@ namespace Mattersight.mock.ba.ae.Tests.Integration
             var serviceId = "unittests";
             using (var silo = new LocalhostSiloBuilder(clusterId, serviceId).Build())
             {
+                Console.WriteLine("Starting Silo");
                 silo.StartAsync().Wait();
+                Console.WriteLine("Silo started");
 
                 var ctx = new CancellationTokenSource();
                 var sut = new Program(new ClusterClientFactory(clusterId, serviceId).CreateOrleansClient().Result, connectionFactory.HostName, connectionFactory.Port);
