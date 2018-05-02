b9258d4d5ef644a24c79bf65bf10f8086c24f330
Greg Biles <greg.biles@mattersight.com>
2018-05-02 12:22:10 -0500

    Added back the async/await in the subscribe between rabbit and orlean streams.

--- src/mock-ae/Ae.cs
+++ src/mock-ae/Ae.cs
@@ -37,7 +37,7 @@ namespace Mattersight.mock.ba.ae
 
                     // Chaing the Rabbit stream to the orleans stream
                     var orleansStream = _orleansClient.GetStreamProvider(Configuration.OrleansStreamProviderName).GetStream<byte[]>(Guid.Empty, StreamNamespaces.TiProducedCallEvents);
-                    _incomingCallEventStream.Subscribe(x => orleansStream.OnNextAsync(x));
+                    _incomingCallEventStream.Subscribe(async x => await orleansStream.OnNextAsync(x));
 
                     do
                     {
